00507872242f565bb4a6cbdbdd35bcb8721cba1b
akashhello <akash.tiwari16@gmail.com>
2018-01-23 00:18:23 -0500

    Logical UsedDisk Display

---
+++ WMI/script.ps1
@@ -0,0 +1,15 @@
+﻿$SystemInfo = @()
+
+$DiskList = GET-WMIOBJECT –query “SELECT * from win32_logicaldisk WHERE DriveType = 3” –computername “LAPTOP-Q2RR0J57”
+#$DiskList|Sort-Object @{e={([int]$_.TotalSpace)}}
+$DiskList = $DiskList|Sort-Object -Property @{Expression={[int]($_.Size/1GB) - [int]($_.FreeSpace/1GB)}}
+
+foreach($Disk in $DiskList)
+{
+    
+    $FreeSpace = "{0:N2}" -f ($Disk.FreeSpace / 1GB)
+	$TotalSpace = "{0:N2}" -f ($Disk.Size / 1GB) 
+    $UsedSpace = [int]$TotalSpace - [int]$FreeSpace;
+    #Write-Output "output" | $UsedSpace $TotalSpace $FreeSpace $Disk.Name
+    Write-Output "UsedSpace:" $UsedSpace "Totalspace:" $TotalSpace "Name:"  $Disk.Name | Format-Table -Auto  
+}
